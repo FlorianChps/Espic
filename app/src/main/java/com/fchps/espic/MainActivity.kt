5d532cce00d4889f02c567ea2e352d8925585af7
Florian Chapuis <florian.chapuis21@gmail.com>
2025-02-01 10:22:03 +0100

    Handle API 26+ permission to write on external storage or we can't capture photo

--- app/src/main/java/com/fchps/espic/MainActivity.kt
+++ app/src/main/java/com/fchps/espic/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.fchps.espic
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.fchps.MainScreen
 import com.fchps.espic.ui.theme.EspicTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +18,18 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                101
+            )
+        }
         enableEdgeToEdge()
         setContent {
             EspicTheme {
